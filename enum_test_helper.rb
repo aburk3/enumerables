50f414326e80aa2eaf0e68419fb905a56afb58d6
Austin Burke <aburke@healthefilings.com>
2020-08-31 12:19:09 -0500

    Updated test helper to handle enumerator class

--- enum_test_helper.rb
+++ enum_test_helper.rb
@@ -1,6 +1,11 @@
 PASSED_MESSAGE = "Test passed"
 
 def show_results(test_name, expected_result, actual_result)
+    if expected_result.class == Enumerator
+        expected_result = expected_result.to_a
+        actual_result = actual_result.to_a
+    end
+    
     if expected_result ==  actual_result
         puts PASSED_MESSAGE
     else
