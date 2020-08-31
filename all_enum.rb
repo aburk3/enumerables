2cb0a248548379c96998b40633698e41bfdc7244
Austin Burke <aburke@healthefilings.com>
2020-08-31 12:19:22 -0500

    Updated all_enum

--- all_enum.rb
+++ all_enum.rb
@@ -7,6 +7,8 @@ MAX_LENGTH = 4
 
 animals = ['cat', 'dog', 'lamb', 'horse']
 
+empty_animals = []
+
 def test_all(arr)
     arr.all? do |element|
         element.length <= MAX_LENGTH
@@ -14,19 +16,18 @@ def test_all(arr)
 end
 
 expected_result = test_all(animals)
+expected_result_2 = test_all(empty_animals)
 
 def animals.all?(*args)
     each do |val|
-        return true if args.size == 1 && args.first == val
-        if block_given? && yield(val)
-            result = true
-        else
-            return false
-        end
+        return args.size == 1 && args.first == val
+        return false if !yield(val)
     end
-    result
+    return true
 end
 
 actual_result = test_all(animals)
+actual_result_2 = test_all(empty_animals)
 
 show_results('test_any', expected_result, actual_result)
+show_results('test_any', expected_result_2, actual_result_2)
