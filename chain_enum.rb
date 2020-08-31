398f4cf21f2a6c91b9dbf7533ce8f38ee85682a8
Austin Burke <aburk3@gmail.com>
2020-08-31 07:22:13 -0500

    Modified code for chain implementation

--- chain_enum.rb
+++ chain_enum.rb
@@ -1,20 +1,24 @@
-# Reimplement #any?
+# Reimplement #chain?
 require 'pry'
 
 require_relative 'enum_test_helper'
 
-animals = ['cat', 'dog', 'lamb', 'hors']
+SPREAD = (1..3)
+array_to_chain = [4,5]
 
-def test_any(arr)
-    
+def test_chain(arr)
+    SPREAD.chain(arr)
 end
 
-expected_result = test_any(animals)
+expected_result = test_chain(array_to_chain)
 
-def (1..3).chain([4,5])
-    
+def SPREAD.chain(arr)
+    combined = SPREAD.to_a + arr
+    combined.each
+    # FAILURE test_chain: 
+    #   Expected #<Enumerator::Chain:0x00007fe7e8963228>, got #<Enumerator:0x00007fe7e8963110>.
 end
 
-actual_result = test_any(animals)
+actual_result = test_chain(array_to_chain)
 
-show_results('test_any', expected_result, actual_result)
+show_results('test_chain', expected_result, actual_result)
