34375035c1706ecfd5646f739577cc14f084ca7f
Austin Burke <aburke@healthefilings.com>
2020-08-31 12:24:42 -0500

    Updated chunking implementation

--- chunk.rb
+++ chunk.rb
@@ -5,7 +5,7 @@ require_relative 'enum_test_helper'
 
 DESIRED_LENGTH = 3
 
-animals = ['raven', 'cat', 'dog', 'lamb', 'hors']
+animals = ['cat', 'dog', 'lamb', 'horse', 'pig']
 
 def test_chunk(arr)
     arr.chunk do |element|
@@ -15,26 +15,28 @@ end
 
 expected_result = test_chunk(animals)
 
-true_results = []
-false_results = []
 def animals.chunk(*args)
-    each do |key,val|
-        binding.pry
-        if yield(val)
-            true_results << val
-            puts [yield(val), true_results]
-            reset_result
-        else
-            false_results << val
-            puts [yield(val), false_results]
-        end
+    grouping = []
+    @all_chunks = []
+    each do |val|
+        # if the new yield is different than the previous, save/clear the current
+        #   grouping to start a new grouping
+        show_and_clear(grouping) if grouping[0] != yield(val) && grouping.length > 0
+        
+        grouping[0] = yield(val) if grouping.length < 1
+        grouping[1] ||= []
+        grouping[1] << val
     end
+    show_and_clear(grouping)
+    @all_chunks.each
 end
 
-def reset_result
-    result.clear
+# Handles adding the current chunk to the rest, and clearing current result
+def show_and_clear(grouping)
+    puts "#{grouping[0]}, #{grouping[1]}"
+    @all_chunks << grouping.dup
+    grouping.clear
 end
 
 actual_result = test_chunk(animals)
-
 show_results('test_chunk', expected_result, actual_result)
