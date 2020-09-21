682777ea6dc9b57cb82a1a0613d60808536bf2f7
Austin Burke <aburke@healthefilings.com>
2020-09-21 09:23:33 -0500

    Updated 'count' enumerable method

--- count_enum.rb
+++ count_enum.rb
@@ -5,24 +5,28 @@ require_relative 'enum_test_helper'
 
 DESIRED_LENGTH = 4
 
-animals = ['cat', 'dog', 'lamb', 'hors']
+array = [1, 2, 4, 2]
 
 def test_count(arr)
-    arr.count do |element|
-        element << 's'
+    arr.count do |x|
+        x % 2
     end
 end
 
-expected_result = test_count(animals)
+expected_result = test_count(array)
 
-def animals.count(*args)
-    @all_results = []
+def array.count(*args)
+    count = 0
     each do |val|
-        @all_results << yield(val)
+        if block_given?
+            count += 1 if yield(val)
+        else
+            count += 1 
+        end
     end
-    @all_results
+    count
 end
 
-actual_result = test_count(animals)
+actual_result = test_count(array)
 
 show_results('test_count', expected_result, actual_result)
