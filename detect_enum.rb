3f0803bd8b654b0bee26d88452b5a1b326b2d2a9
Austin Burke <aburke@healthefilings.com>
2020-10-07 16:36:05 -0500

    Finished detect_enum

--- detect_enum.rb
+++ detect_enum.rb
@@ -5,32 +5,27 @@ require_relative 'enum_test_helper'
 
 DESIRED_LENGTH = 4
 
-list = [1..100]
+list = (1..10)
 
 def test_detect(arr)
-    arr.detect(3) do |x|
-        puts x
+    arr.detect do |x|
+        x % 2 == 0
     end
 end
 
 expected_result = test_detect(list)
 
-def list.detect(*args)
+def list.detect(ifnone = nil)
     each do |val|
-        # Return enumerator object if no block is given
+        # If no block is given, an enumerator is returned instead.
         return self.each if !block_given?
-        # Do nothing if arg is non-positive or collection is empty
-        return if args[0] <= 0 || self.length < 1
-        # Infinite loop if none or nil is given
-        loop { yield(val) } if args.none? || args == nil
-        # Loop the specified # of times
-        while count < args[0] do 
-            yield(val)
-            count += 1
-        end
-
+        puts val if yield(val)
+        # Returns the first for which block is not false.
+        return val if yield(val)
     end
-    # Returns nil if loop has finished without interruption
+    # If no object matches, calls ifnone and returns its result when it is specified
+    return send(ifnone) if ifnone
+    # or returns nil otherwise.
     nil
 end
 
