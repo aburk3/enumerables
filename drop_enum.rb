7657a696b6107f352431fc99670f323bae54630f
Austin Burke <aburke@healthefilings.com>
2020-10-19 10:16:28 -0500

    Refactor drop enumerable

--- drop_enum.rb
+++ drop_enum.rb
@@ -8,7 +8,7 @@ DESIRED_LENGTH = 4
 my_arr = [1, 2, 3, 4, 5, 0]
 
 def test_drop(arr)
-    arr.drop(5)
+    arr.drop(3)
 end
 
 expected_result = test_drop(my_arr)
@@ -16,10 +16,13 @@ expected_result = test_drop(my_arr)
 def my_arr.drop(*args)
     raise "wrong number of arguments (given #{args.count}, expected 1)" if args.count != 1
     number_to_remove = args[0].to_i
-    args[0].to_i.times do |element|
-        self.shift
+    dropped_elements = []
+
+    number_to_remove.times do |index|
+        dropped_elements << self[index]
     end
-    return self
+    
+    return self - dropped_elements
 end
 
 actual_result = test_drop(my_arr)
