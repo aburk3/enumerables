f2a615c23c2878969aad7e0a1b6c9b283ef5f76a
Austin Burke <aburke@healthefilings.com>
2020-11-10 08:26:18 -0600

    Added entries_enum code

---
+++ entries_enum.rb
@@ -0,0 +1,26 @@
+# Reimplement #entries
+require 'pry'
+require 'prime'
+require_relative 'enum_test_helper'
+
+#* Example: https://www.geeksforgeeks.org/ruby-range-entries-function/
+
+range = (0..10)
+
+def test_entries(range)
+    range.entries()
+end
+
+expected_result = test_entries(range)
+
+def range.entries(*args)
+  my_arr = []
+  self.each do |element|
+    my_arr << element
+  end
+  my_arr
+end
+
+actual_result = test_entries(range)
+
+show_results('test_entries', expected_result, actual_result)
