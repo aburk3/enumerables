ff248c0095ab82a029e9b2e903e255b27c58ff93
Austin Burke <aburke@healthefilings.com>
2020-12-09 16:55:35 -0600

    Added partition enum

---
+++ partition.rb
@@ -0,0 +1,40 @@
+# Reimplement #partition
+require 'pry'
+require 'prime'
+require_relative 'enum_test_helper'
+
+#* https://ruby-doc.org/core-2.6/Enumerable.html#method-i-partition
+
+my_arr = %w(ant bear cat anima)
+
+def test_partition(elements)
+  elements.partition do |word|
+    word.length == 5
+  end
+end
+
+# Test before overwriting partition method
+expected_result = test_partition(my_arr)
+
+def my_arr.partition(*args)
+  return enum_for(:partition) unless block_given?
+
+  true_arr = []
+  false_arr = []
+
+  self.each do |element|
+    if yield element
+      true_arr << element
+    else
+      false_arr << element
+    end
+  end
+
+  return [true_arr, false_arr]
+end
+
+# Test after overwriting partition method
+actual_result = test_partition(my_arr)
+
+# Comparing results
+show_results('test_partition', expected_result, actual_result)
